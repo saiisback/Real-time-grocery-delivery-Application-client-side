fe4eec2c83cc62d05de38aa78508791837c3667d
sai karthik <69042752+saiisback@users.noreply.github.com>
2024-08-03 10:26:20 +0000

    update 0.4

--- grocery-online-store/src/components/Header/Header.tsx
+++ grocery-online-store/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { LayoutGrid } from 'lucide-react';
+import { LayoutGrid, Search } from 'lucide-react';
 import Image from 'next/image';
 import React from 'react';
 
@@ -8,12 +8,15 @@ type HeaderProps = {
 
 const Header:React.FC<HeaderProps> = () => {
     
-    return <div>
-        <div className='flex gap-2'>
+    return <div className='p-5 shadow-md'>
+        <div className='flex gap-8 items-center px-5'>
             <Image src="/BIXBY.png" alt="logo" width={100} height={100} />
-            <div>
-                <h2 className='flex gap-2 items-center border rounded-full p-2 px-10 bg-slate-200'> <LayoutGrid className='h-5 w-5'/> Category</h2>
-            </div>
+                <h2 className='flex gap-2 items-center border rounded-full p-2 px-10 bg-gray-100 border-black '> <LayoutGrid className='h-5 w-5'/> Category</h2>
+                <div  className='md:flex gap-3 items-center border rounded-full p-2 px-5
+                hidden'>
+                    <Search/>
+                    <input type='text' placeholder='Search' className='outline-none'/>
+                </div>
         </div>
     </div>
 }
